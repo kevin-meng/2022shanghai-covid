6ea9d037e3107f3dd2b26f923d50a10f9849639d
kevin777 <meng_fr595@163.com>
2022-04-09 12:21:11 +0800

    update new content

--- app.py
+++ app.py
@@ -78,3 +78,4 @@ for app in apps:
 # analytics
 st.components.v1.iframe('https://github.com/kevin-meng/2022shanghai-covid/blob/main/template/baidu_analytics.html', height=1, scrolling=False)
 
+st.components.v1.iframe('https://github.com/kevin-meng/2022shanghai-covid/blob/main/template/analytics.html', height=1, scrolling=False)
